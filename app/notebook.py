1d805293065145c992e07f19b63fefaef6704eb7
Moshua53 <moisesjoshua53@gmail.com>
2025-02-18 11:24:04 -0500

    IniciandoClassNotebook

--- app/notebook.py
+++ app/notebook.py
@@ -18,6 +18,18 @@ class Note:
     def fecha_creacion(self):
         return f"{self.creation_date}-{self.title}"
 
+    class Notebook:
+        #Atributos
+        def __init__(self):
+            self.notes:list[Note]=[]
+        #Metodos
+        Numero_notas = int(len(self.notes))
+        def add_note(self,title:str,text:str,importance:str)-> int:
+            code=Numero_notas+
+            
+
+
+
 
 
 
